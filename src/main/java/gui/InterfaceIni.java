7c12d4779e00bb90d3eec7167b05ec5ba6414f02
CurtisNewbie <zhuangyongj@gmail.com>
2020-01-04 18:33:26 +0000

    Move CalculatorGui.fxml into resources folder
    CalculatorGui.fxml is now moved under the resources folder, and this file is loaded using:
    
    	getClass().getClassLoader().getResource("CalculatorGui.fxml")
    
    modified:   src/main/java/gui/InterfaceIni.java
    renamed:    src/main/java/gui/CalculatorGui.fxml -> src/main/resources/CalculatorGui.fxml

--- src/main/java/gui/InterfaceIni.java
+++ src/main/java/gui/InterfaceIni.java
@@ -7,29 +7,15 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class InterfaceIni extends Application {
-    
-    public InterfaceIni() {
-	// TODO Auto-generated constructor stub
-    }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-	Parent root = FXMLLoader.load(this.getClass().getResource("CalculatorGui.fxml"));
-	Scene scene = new Scene(root);
-	primaryStage.setScene(scene);
-	primaryStage.setTitle("Very Basic, Simple Calculator");
-	primaryStage.setResizable(false);
-	primaryStage.show();
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("CalculatorGui.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Very Basic, Simple Calculator");
+		primaryStage.setResizable(false);
+		primaryStage.show();
 
-    }
-
-    public static void main(String[] args) {
-	try {
-	    InterfaceIni ui = new InterfaceIni();
-	    ui.launch(args);
-	} catch (Exception e) {
-	    System.out.println(e.getMessage());
 	}
-    }
-
 }
