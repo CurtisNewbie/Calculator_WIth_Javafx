2c5d0c87a2bbf44f1c9be8a06cd180bcbd935623
CurtisNewbie <zhuangyongj@gmail.com>
2020-01-04 18:42:20 +0000

    Formatting code (e.g., indentation)
    modified:   .classpath
    new file:   .settings/org.eclipse.jdt.apt.core.prefs
    modified:   .settings/org.eclipse.jdt.core.prefs
    modified:   src/main/java/gui/InterfaceController.java
    modified:   src/main/java/model/CalculationAction.java

--- src/main/java/gui/InterfaceController.java
+++ src/main/java/gui/InterfaceController.java
@@ -10,159 +10,159 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import model.CalculationAction;
 
-public class InterfaceController implements Initializable{
+public class InterfaceController implements Initializable {
 
-    @FXML
-    private Button num1;
-    @FXML
-    private Button num2;
-    @FXML
-    private Button num3;
-    @FXML
-    private Button num4;
-    @FXML
-    private Button num5;
-    @FXML
-    private Button num6;
-    @FXML
-    private Button num7;
-    @FXML
-    private Button num8;
-    @FXML
-    private Button num9;
-    @FXML
-    private Button btnCls;
-    @FXML
-    private Button btnBackSpace;
-    @FXML
-    private Button btnDivide;
-    @FXML
-    private Button btnSub;
-    @FXML
-    private Button btnAdd;
-    @FXML
-    private Button btnEqual;
-    @FXML
-    private Button btnMultiply;
-    @FXML
-    private Label labelResult;
+	@FXML
+	private Button num1;
+	@FXML
+	private Button num2;
+	@FXML
+	private Button num3;
+	@FXML
+	private Button num4;
+	@FXML
+	private Button num5;
+	@FXML
+	private Button num6;
+	@FXML
+	private Button num7;
+	@FXML
+	private Button num8;
+	@FXML
+	private Button num9;
+	@FXML
+	private Button btnCls;
+	@FXML
+	private Button btnBackSpace;
+	@FXML
+	private Button btnDivide;
+	@FXML
+	private Button btnSub;
+	@FXML
+	private Button btnAdd;
+	@FXML
+	private Button btnEqual;
+	@FXML
+	private Button btnMultiply;
+	@FXML
+	private Label labelResult;
 
-    private StringBuilder query;
+	private StringBuilder query;
 
-    // Calculate the numbers based on the entered query.
-    public void calculate(ActionEvent e) {
-	if (query.charAt(query.length() - 1) != ' ') {
-	    String result = CalculationAction.parseQuery(query.toString());
-	    labelResult.setText(result);
-	}
-    }
-
-    // Click the numbers buttons
-    public void enterNumbers(ActionEvent e) {
-	// get the button's id
-	String id = ((Button) e.getSource()).getId();
-	switch (id) {
-	case "num1":
-	    query.append("1");
-	    break;
-	case "num2":
-	    query.append("2");
-	    break;
-	case "num3":
-	    query.append("3");
-	    break;
-	case "num4":
-	    query.append("4");
-	    break;
-	case "num5":
-	    query.append("5");
-	    break;
-	case "num6":
-	    query.append("6");
-	    break;
-	case "num7":
-	    query.append("7");
-	    break;
-	case "num8":
-	    query.append("8");
-	    break;
-	case "num9":
-	    query.append("9");
-	    break;
-	default:
-	    break;
+	// Calculate the numbers based on the entered query.
+	public void calculate(ActionEvent e) {
+		if (query.charAt(query.length() - 1) != ' ') {
+			String result = CalculationAction.parseQuery(query.toString());
+			labelResult.setText(result);
+		}
 	}
 
-	// Update display
-	labelResult.setText(query.toString());
-    }
+	// Click the numbers buttons
+	public void enterNumbers(ActionEvent e) {
+		// get the button's id
+		String id = ((Button) e.getSource()).getId();
+		switch (id) {
+		case "num1":
+			query.append("1");
+			break;
+		case "num2":
+			query.append("2");
+			break;
+		case "num3":
+			query.append("3");
+			break;
+		case "num4":
+			query.append("4");
+			break;
+		case "num5":
+			query.append("5");
+			break;
+		case "num6":
+			query.append("6");
+			break;
+		case "num7":
+			query.append("7");
+			break;
+		case "num8":
+			query.append("8");
+			break;
+		case "num9":
+			query.append("9");
+			break;
+		default:
+			break;
+		}
 
-    // Click the operators.
-    public void selectOperator(ActionEvent e) {
-
-	if (canAddOperator()) {
-	    // get the button's id
-	    String id = ((Button) e.getSource()).getId();
-	    switch (id) {
-	    case "btnDivide":
-		query.append(" / ");
-		break;
-	    case "btnSub":
-		query.append(" - ");
-		break;
-	    case "btnAdd":
-		query.append(" + ");
-		break;
-	    case "btnMultiply":
-		query.append(" * ");
-		break;
-	    default:
-		break;
-	    }
-	    // Update display
-	    labelResult.setText(query.toString());
+		// Update display
+		labelResult.setText(query.toString());
 	}
-    }
 
-    // remove the last character
-    public void backspace(ActionEvent e) {
-	int queryLen = query.length();
-	if (queryLen > 0) {
-	    if (query.charAt(queryLen - 1) == ' ') {
-		query.delete(queryLen - 3, queryLen);
-	    } else {
-		query.deleteCharAt(queryLen - 1);
-	    }
-	    // Update display
-	    labelResult.setText(query.toString());
+	// Click the operators.
+	public void selectOperator(ActionEvent e) {
+
+		if (canAddOperator()) {
+			// get the button's id
+			String id = ((Button) e.getSource()).getId();
+			switch (id) {
+			case "btnDivide":
+				query.append(" / ");
+				break;
+			case "btnSub":
+				query.append(" - ");
+				break;
+			case "btnAdd":
+				query.append(" + ");
+				break;
+			case "btnMultiply":
+				query.append(" * ");
+				break;
+			default:
+				break;
+			}
+			// Update display
+			labelResult.setText(query.toString());
+		}
 	}
-    }
 
-    // clean screen
-    public void clean(ActionEvent e) {
-	query = new StringBuilder();
-	// Update display
-	labelResult.setText(query.toString());
-    }
-
-    // it is used to verify whether there is aleady an operator at the end of the
-    // string.
-    private boolean canAddOperator() {
-	if (query.length() != 0) {
-	    if (query.charAt(query.length() - 1) == ' ') {
-		return false;
-	    } else {
-		return true;
-	    }
-	} else {
-	    return false;
+	// remove the last character
+	public void backspace(ActionEvent e) {
+		int queryLen = query.length();
+		if (queryLen > 0) {
+			if (query.charAt(queryLen - 1) == ' ') {
+				query.delete(queryLen - 3, queryLen);
+			} else {
+				query.deleteCharAt(queryLen - 1);
+			}
+			// Update display
+			labelResult.setText(query.toString());
+		}
 	}
-    }
 
-    @Override
-    public void initialize(URL arg0, ResourceBundle arg1) {
-	query = new StringBuilder();
-	labelResult.setText("Start by selecting numbers or operators.");
-    }
+	// clean screen
+	public void clean(ActionEvent e) {
+		query = new StringBuilder();
+		// Update display
+		labelResult.setText(query.toString());
+	}
+
+	// it is used to verify whether there is aleady an operator at the end of the
+	// string.
+	private boolean canAddOperator() {
+		if (query.length() != 0) {
+			if (query.charAt(query.length() - 1) == ' ') {
+				return false;
+			} else {
+				return true;
+			}
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		query = new StringBuilder();
+		labelResult.setText("Start by selecting numbers or operators.");
+	}
 
 }
--- src/main/java/model/CalculationAction.java
+++ src/main/java/model/CalculationAction.java
@@ -6,101 +6,101 @@ import java.util.StringTokenizer;
 
 public class CalculationAction {
 
-    private final static String ADD_OPER = "+";
-    private final static String SUB_OPER = "-";
-    private final static String MUL_OPER = "*";
-    private final static String DIV_OPER = "/";
+	private final static String ADD_OPER = "+";
+	private final static String SUB_OPER = "-";
+	private final static String MUL_OPER = "*";
+	private final static String DIV_OPER = "/";
 
-    // This should be recursive.
-    public static String parseQuery(String query) {
-	StringTokenizer strToken = new StringTokenizer(query, " ");
-	int numOfToken = strToken.countTokens();
-	List<String> list = new LinkedList<>();
-	for (int x = 0; x < numOfToken; x++) {
-	    list.add(strToken.nextToken());
-	}
-
-	String str;
-	boolean loopFinished = false; // when it identifies an operator, check the whole list one more time.
-	while (!loopFinished) {
-	    loopFinished = true;
-	    // first loop detecting multiplication and division
-	    for (int x = 0; x < list.size(); x++) {
-		str = list.get(x);
-		if (isOperator(str)) {
-		    String thisResult;
-		    if (str.equals(MUL_OPER)) {
-			thisResult = multiply(Double.parseDouble(list.get(x - 1)), Double.parseDouble(list.get(x + 1)));
-			list.set(x, thisResult);
-			list.remove(x - 1);
-			list.remove(x);
-			loopFinished = false;
-		    } else if (str.equals(DIV_OPER)) {
-			thisResult = divide(Double.parseDouble(list.get(x - 1)), Double.parseDouble(list.get(x + 1)));
-			list.set(x, thisResult);
-			list.remove(x - 1);
-			list.remove(x);
-			loopFinished = false;
-		    }
+	// This should be recursive.
+	public static String parseQuery(String query) {
+		StringTokenizer strToken = new StringTokenizer(query, " ");
+		int numOfToken = strToken.countTokens();
+		List<String> list = new LinkedList<>();
+		for (int x = 0; x < numOfToken; x++) {
+			list.add(strToken.nextToken());
 		}
-	    }
-	}
 
-	loopFinished = false;
-	while (!loopFinished) {
-	    loopFinished = true;
-	    // second loop detecting addition and subtraction
-	    for (int x = 0; x < list.size(); x++) {
-		str = list.get(x);
-
-		if (isOperator(str)) {
-		    String thisResult;
-		    if (str.equals(ADD_OPER)) {
-			thisResult = add(Double.parseDouble(list.get(x - 1)), Double.parseDouble(list.get(x + 1)));
-			list.set(x, thisResult);
-			list.remove(x - 1);
-			list.remove(x);
-			loopFinished = false;
-		    } else if (str.equals(SUB_OPER)) {
-			thisResult = subtract(Double.parseDouble(list.get(x - 1)), Double.parseDouble(list.get(x + 1)));
-			list.set(x, thisResult);
-			list.remove(x - 1);
-			list.remove(x);
-			loopFinished = false;
-		    }
+		String str;
+		boolean loopFinished = false; // when it identifies an operator, check the whole list one more time.
+		while (!loopFinished) {
+			loopFinished = true;
+			// first loop detecting multiplication and division
+			for (int x = 0; x < list.size(); x++) {
+				str = list.get(x);
+				if (isOperator(str)) {
+					String thisResult;
+					if (str.equals(MUL_OPER)) {
+						thisResult = multiply(Double.parseDouble(list.get(x - 1)), Double.parseDouble(list.get(x + 1)));
+						list.set(x, thisResult);
+						list.remove(x - 1);
+						list.remove(x);
+						loopFinished = false;
+					} else if (str.equals(DIV_OPER)) {
+						thisResult = divide(Double.parseDouble(list.get(x - 1)), Double.parseDouble(list.get(x + 1)));
+						list.set(x, thisResult);
+						list.remove(x - 1);
+						list.remove(x);
+						loopFinished = false;
+					}
+				}
+			}
 		}
-	    }
+
+		loopFinished = false;
+		while (!loopFinished) {
+			loopFinished = true;
+			// second loop detecting addition and subtraction
+			for (int x = 0; x < list.size(); x++) {
+				str = list.get(x);
+
+				if (isOperator(str)) {
+					String thisResult;
+					if (str.equals(ADD_OPER)) {
+						thisResult = add(Double.parseDouble(list.get(x - 1)), Double.parseDouble(list.get(x + 1)));
+						list.set(x, thisResult);
+						list.remove(x - 1);
+						list.remove(x);
+						loopFinished = false;
+					} else if (str.equals(SUB_OPER)) {
+						thisResult = subtract(Double.parseDouble(list.get(x - 1)), Double.parseDouble(list.get(x + 1)));
+						list.set(x, thisResult);
+						list.remove(x - 1);
+						list.remove(x);
+						loopFinished = false;
+					}
+				}
+			}
+		}
+		return list.get(0);
 	}
-	return list.get(0);
-    }
 
-    // add up num1 and num2
-    public static String add(double num1, double num2) {
-	return "" + (num1 + num2);
-    }
-
-    // Subtract num1 from num2
-    public static String subtract(double num1, double num2) {
-	return "" + (num1 - num2);
-    }
-
-    // Multiply num1 by num2
-    public static String multiply(double num1, double num2) {
-	return "" + (num1 * num2);
-    }
-
-    // Divide num1 by num2
-    public static String divide(double num1, double num2) {
-	return "" + (num1 / num2);
-    }
-
-    // is operator
-    private static boolean isOperator(String str) {
-	if (str.matches("\\d{1,}")) {
-	    return false;
-	} else {
-	    return true;
+	// add up num1 and num2
+	public static String add(double num1, double num2) {
+		return "" + (num1 + num2);
 	}
-    }
+
+	// Subtract num1 from num2
+	public static String subtract(double num1, double num2) {
+		return "" + (num1 - num2);
+	}
+
+	// Multiply num1 by num2
+	public static String multiply(double num1, double num2) {
+		return "" + (num1 * num2);
+	}
+
+	// Divide num1 by num2
+	public static String divide(double num1, double num2) {
+		return "" + (num1 / num2);
+	}
+
+	// is operator
+	private static boolean isOperator(String str) {
+		if (str.matches("\\d{1,}")) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 
 }
